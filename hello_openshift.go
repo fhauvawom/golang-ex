dfc005988c0f8c6c14502f8e91512f429fb3574f
fhauvawom <82159461+fhauvawom@users.noreply.github.com>
2022-03-25 21:07:33 -0300

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -8,6 +8,19 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	var span opentracing.Span
+
+	// The HTTP instrumentation injects an entry span into request context, so it can be used
+	// as a parent for any operation resulted from an incoming HTTP request. Here we're checking
+	// whether the parent span present in request context, which means that our handler has been
+	// instrumented.
+	if parent, ok := instana.SpanFromContext(req.Context()); ok {
+		// Since our handler does some substantial "work", we'd like to have more visibility
+		// on how much time it takes to process a request. For this we're starting an _intermediate_
+		// span that will be finished as soon as handling is finished.
+		span = parent.Tracer().StartSpan("handle", opentracing.ChildOf(parent.Context()))
+		defer span.Finish()
+	}
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
 		response = "Hello OpenShift!"
